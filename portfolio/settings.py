16140d52b01f4175e5a83f04d672d74126a313f1
mekeltensley <mekelintech@gmail.com>
2021-06-23 19:31:25 -0500

    Update Hosts

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_TWO')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['radiant-depths-60506.herokuapp.com']
 
 # Application definition
 
